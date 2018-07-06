9a652981bdd8d47918cb6d21040bfa9c5232fdc6
Mykola <mykola.krys@kindgeek.com>
2018-07-06 17:39:47 +0300

    new script

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = next({ dev: isDev });
 const handler = routes.getRequestHandler(app);
 
 // With express
+console.log(process.env.NODE_ENV);
 const express = require('express');
 const port = isDev ? 3000 : 80;
 app.prepare().then(() => {
