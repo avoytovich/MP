146f82237eceea169a0c497b1773569c4f57b694
Mykola <mykola.krys@kindgeek.com>
2018-08-01 11:55:04 +0300

    added icons

--- pages/_document.js
+++ pages/_document.js
@@ -29,6 +29,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
           />
           <link rel="stylesheet" href="/_next/static/style.css" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
         </Head>
         <body>
           <Main />
